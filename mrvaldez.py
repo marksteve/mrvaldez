67b33d543fd2dbeef6dea14902fe122b575f7011
Mark Steve Samson <marksteve@insynchq.com>
2012-07-02 12:24:03 +0800

    Fixed cartwheel gif url

--- mrvaldez.py
+++ mrvaldez.py
@@ -3,4 +3,4 @@ HOST = 'marksteve.github.com'
 
 def cartwheel():
   import webbrowser
-  webbrowser.open_new_tab('http://marksteve.github.com/mrvaldez/cartwheel.gif')
+  webbrowser.open_new_tab('http://marksteve.github.com/mrvaldez/images/cartwheel.gif')
