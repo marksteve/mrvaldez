993289e237172256fb336d92e6cf1c0fb464891f
Mark Steve Samson <marksteve@insynchq.com>
2012-07-02 11:58:32 +0800

    Added setup.py and main module

---
+++ mrvaldez.py
@@ -0,0 +1,6 @@
+HOST = 'marksteve.github.com'
+
+
+def cartwheel():
+  import webbrowser
+  webbrowser.open_new_tab('http://marksteve.github.com/mrvaldez/cartwheel.gif')
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+  name='Mr_Valdez',
+  version='0.1.0',
+  py_modules=['mrvaldez'],
+  author='Mark Steve Samson',
+  author_email='contact@marksteve.me',
+  description='Mr. Valdez has no first name',
+  install_requires=[
+    # What requirements?
+    # Mr. Valdez requires nothing
+  ],
+)
