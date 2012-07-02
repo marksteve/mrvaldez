9d646fbfd9771bf566df1cf8487fcecebc562fc5
Mark Steve Samson <marksteve@insynchq.com>
2012-07-02 12:25:21 +0800

    Forgot to use a constant :P

--- mrvaldez.py
+++ mrvaldez.py
@@ -3,4 +3,4 @@ HOST = 'marksteve.github.com'
 
 def cartwheel():
   import webbrowser
-  webbrowser.open_new_tab('http://marksteve.github.com/mrvaldez/images/cartwheel.gif')
+  webbrowser.open_new_tab('http://%s/mrvaldez/images/cartwheel.gif' % HOST)
